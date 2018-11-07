554ddf0522fc8b09e750bfbcb467679349a89564
jdtolentino <jltolentino0106@gmail.com>
2018-11-08 02:16:48 +0800

    Update application/controllers/Article.php

--- application/controllers/Article.php
+++ application/controllers/Article.php
@@ -5,35 +5,50 @@ class Article extends CI_Controller {
 
     public function index()
 	{
-        // if(!empty($this->session->userdata['user'])){ // if has session
-        //     if($this->session->userdata['user']->user_type == 'student'){ // if user type student 
-		// 		// load view
-        //         // $data['articleList'] = $this->admin_model->articleTypeList();
-		// 		$this->load->view('templates/header');
-        //         $this->load->view('article/article');
-        //         $this->load->view('templates/footer');
-		// 	} else { 
-		// 		show_404(); // show 404 error page
-		// 	}
-		// } else {
-		// 	show_404(); // show 404 error page
-        // }
-        
-        $this->load->view('templates/header');
-        $this->load->view('article/dashboard');
-        $this->load->view('templates/footer');
+        if(!empty($this->session->userdata['user'])){ // if has session
+            if($this->session->userdata['user']->user_type == 'student'){ // if user type student 
+				// load view
+                $this->load->view('templates/header');
+                $this->load->view('article/dashboard');
+                $this->load->view('templates/footer');
+			} else { 
+				show_404(); // show 404 error page
+			}
+		} else {
+			show_404(); // show 404 error page
+        }
+     
     }
     public function articles(){
-        $data['articles'] = $this->article_model->getArticles();
-        $data['articleType'] = ucwords($_GET['type']);
-        $this->load->view('templates/header');
-        $this->load->view('article/article',$data);
-        $this->load->view('templates/footer');
+        if(!empty($this->session->userdata['user'])){ // if has session
+            if($this->session->userdata['user']->user_type == 'student'){ // if user type student 
+				// load view
+                $data['articles'] = $this->article_model->getArticles();
+                $data['articleType'] = ucwords($_GET['type']);
+                $this->load->view('templates/header');
+                $this->load->view('article/article',$data);
+                $this->load->view('templates/footer');
+			} else { 
+				show_404(); // show 404 error page
+			}
+		} else {
+			show_404(); // show 404 error page
+        }
     }
     public function view(){
-        $data['article'] = $this->article_model->getArticles($_GET['id']);
-        $this->load->view('templates/header');
-        $this->load->view('article/view',$data);
-        $this->load->view('templates/footer');
+        if(!empty($this->session->userdata['user'])){ // if has session
+            if($this->session->userdata['user']->user_type == 'student'){ // if user type student 
+				// load view
+                $data['article'] = $this->article_model->getArticles($_GET['id']);
+                $this->load->view('templates/header');
+                $this->load->view('article/view',$data);
+                $this->load->view('templates/footer');
+			} else { 
+				show_404(); // show 404 error page
+			}
+		} else {
+			show_404(); // show 404 error page
+        }
+       
     }
 }
