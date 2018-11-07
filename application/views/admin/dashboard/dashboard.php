766c5eacea0dbab0b3850fc51f809c08d6a3d729
YzahFe <khariza.gapuz17@gmail.com>
2018-11-08 02:20:35 +0800

    Update application/views/admin/dashboard/dashboard.php

--- application/views/admin/dashboard/dashboard.php
+++ application/views/admin/dashboard/dashboard.php
@@ -12,7 +12,7 @@
     </div>
     <div class="col-md-6">
         <div class="card text-white bg-dark mb-3 pt-1">
-            <div class="card-header text-center"><h6>Number of aricles submitted</h6></div>
+            <div class="card-header text-center"><h6>Number of articles submitted</h6></div>
             <div class="card-body">
                 <h1 class="card-title text-center"><?= count($total2)?></h1>
             </div>
